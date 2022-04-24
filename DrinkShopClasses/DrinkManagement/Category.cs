7e40033f3e4ea615f7b38f27e6e5eb5d3900223d
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2022-04-24 20:13:53 +0200

    🎉 Add : Drinks Category Model

---
+++ DrinkShopClasses/DrinkManagement/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkShopClasses.DrinkManagement
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Display(Name = "Drink Category")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Drink> Drinks { get; set; }
+    }
+}
