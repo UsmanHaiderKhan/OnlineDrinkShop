f1ef3aa9710635dc0427f1ee92b06eeb1e330b04
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2022-04-25 20:17:28 +0200

    🎉 Add : Orders model with their properties

---
+++ DrinkShopClasses/DrinkManagement/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkShopClasses.DrinkManagement
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string BuyerName { get; set; }
+        public string EmailAddress { get; set; }
+        public string FullAddress { get; set; }
+        public bool IsActive { get; set; }
+        public double Phone { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
+
+    }
+}
