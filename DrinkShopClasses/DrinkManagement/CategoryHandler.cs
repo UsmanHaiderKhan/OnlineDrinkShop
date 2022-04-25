91a201759e49e2398f35f275bfa1f417bd52dbc7
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2022-04-25 20:15:54 +0200

    🎉 Add : Category handler to get the category list item

---
+++ DrinkShopClasses/DrinkManagement/CategoryHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkShopClasses.DrinkManagement
+{
+    public class CategoryHandler
+    {
+        DrinkShopContext db = new DrinkShopContext();
+
+        public List<Category> GetAllCategories()
+        {
+            using (db)
+            {
+                return (from c in db.Categories select c).ToList();
+            }
+        }
+    }
+}
