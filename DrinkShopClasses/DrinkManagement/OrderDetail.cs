a486245c860efb47bfd692c3dfe2fe64bacb82d7
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2022-04-29 20:19:42 +0200

    🎉 Add : Order Model with order details properties

---
+++ DrinkShopClasses/DrinkManagement/OrderDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkShopClasses.DrinkManagement
+{
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string ImageUrl { get; set; }
+        public int Qauntity { get; set; }
+        public long Amount { get; set; }
+    }
+}
