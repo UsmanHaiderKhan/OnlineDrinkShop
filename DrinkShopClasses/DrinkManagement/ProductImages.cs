565b1bda8466a62210cf79b6e8dcd23912687e62
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2022-04-29 20:20:50 +0200

    🎉 Add : Product images model

---
+++ DrinkShopClasses/DrinkManagement/ProductImages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkShopClasses.DrinkManagement
+{
+    public class ProductImages
+    {
+        public int Id { get; set; }
+        public string Caption { get; set; }
+        public int Perority { get; set; }
+        public string Url { get; set; }
+    }
+}
