d3c8e749315581ba585db58c462e364c4791b0f4
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2022-05-01 20:24:50 +0200

    🎉 Add : User Handler with all the get methods

---
+++ DrinkShopClasses/UserMangement/UserHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkShopClasses.UserMangement
+{
+    public class UserHandler
+    {
+        DrinkShopContext db = new DrinkShopContext();
+        public List<User> GetUsers()
+        {
+            using (db)
+            {
+                return (from c in db.Users.Include(m => m.Role) select c).ToList();
+            }
+        }
+
+        public User GetUser(string userName, string password)
+        {
+            using (db)
+            {
+                return (from c in db.Users.Include(m => m.Role)
+                        where c.UserName.Equals(userName) && c.Password.Equals(password)
+                        select c).FirstOrDefault();
+            }
+        }
+        public User GetUserById(int id)
+        {
+            DrinkShopContext db = new DrinkShopContext();
+            using (db)
+            {
+                return (from c in db.Users
+                        .Include(x => x.Role)
+                        where c.Id == id
+                        select c).FirstOrDefault();
+            }
+        }
+        public User GetUserByEmail(string email)
+        {
+            DrinkShopContext db = new DrinkShopContext();
+            using (db)
+            {
+                return (from c in db.Users where c.Email == email select c).FirstOrDefault();
+            }
+        }
+    }
+}
