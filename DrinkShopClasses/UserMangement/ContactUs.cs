ebc9e95ba79b3bf95841b3cfdda70fb803907a75
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2022-05-01 20:24:10 +0200

    🎉 Add : Contact Model with contact details attributes

---
+++ DrinkShopClasses/UserMangement/ContactUs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrinkShopClasses.UserMangement
+{
+    public class ContactUs
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string CompanyName { get; set; }
+        public long Phone { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+
+    }
+}
