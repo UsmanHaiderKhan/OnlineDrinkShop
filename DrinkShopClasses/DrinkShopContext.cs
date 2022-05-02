72a068385b23b3a75407af3bacba015bf6e67d36
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2022-05-02 18:49:16 +0200

    🎉 Add : solution file

---
+++ DrinkShopClasses/DrinkShopContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DrinkShopClasses.DrinkManagement;
+using DrinkShopClasses.UserMangement;
+
+namespace DrinkShopClasses
+{
+    public class DrinkShopContext : DbContext
+    {
+        public DrinkShopContext() : base("name=constr")
+        {
+
+        }
+        public DbSet<Drink> Drinks { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductImages> ProductImageses { get; set; }
+        public DbSet<OrderStatus> OrderStatuses { get; set; }
+        public DbSet<ContactUs> Contacts { get; set; }
+
+    }
+}
