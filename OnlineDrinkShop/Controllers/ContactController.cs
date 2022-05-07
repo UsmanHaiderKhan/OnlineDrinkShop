423da71551bd709f28df7d5962b50a2e742fa789
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2022-05-07 20:28:06 +0200

    🎉 Add : Contact controller with index method and send method

---
+++ OnlineDrinkShop/Controllers/ContactController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DrinkShopClasses;
+using DrinkShopClasses.UserMangement;
+
+namespace OnlineDrinkShop.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult Index(ContactUs contactus)
+        {
+            DrinkShopContext db = new DrinkShopContext();
+            using (db)
+            {
+                db.Contacts.Add(contactus);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Send");
+        }
+
+        public ActionResult Send()
+        {
+            return View();
+        }
+    }
+}
