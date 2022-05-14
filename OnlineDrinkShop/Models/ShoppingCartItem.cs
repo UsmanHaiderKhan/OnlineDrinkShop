db310f276d12833b03d26a78cad3ad9ee96e4251
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2022-05-14 19:15:15 +0200

    🎉✨ Add : Shopping cart item model

---
+++ OnlineDrinkShop/Models/ShoppingCartItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineDrinkShop.Models
+{
+    public class ShoppingCartItem
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public float Price { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int Amount
+        {
+            get
+            {
+                return (int)(Price * Quantity);
+            }
+        }
+    }
+}
