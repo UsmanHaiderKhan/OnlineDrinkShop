6b05a76404fa632ea5072cf6abe40b03580eb501
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2022-05-15 19:39:57 +0200

    🎉 Add : Drink List View Model

---
+++ OnlineDrinkShop/Models/DrinkListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using DrinkShopClasses.DrinkManagement;
+using System.Collections.Generic;
+
+namespace OnlineDrinkShop.Models
+{
+    public class DrinkListViewModel
+    {
+        public string CurrentCategory { get; set; }
+        public IEnumerable<Drink> Drinks { get; set; }
+    }
+}
