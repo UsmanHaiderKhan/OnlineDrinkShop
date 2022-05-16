f484047398b2f199ed1b18af42d5267b05b0dd6b
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2022-05-16 19:40:48 +0200

    🎉 Add : Email Model with email format and validation

---
+++ OnlineDrinkShop/Models/Email.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineDrinkShop.Models
+{
+    public class Email
+    {
+        [Required]
+        [EmailAddress()]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string email { get; set; }
+    }
+}
