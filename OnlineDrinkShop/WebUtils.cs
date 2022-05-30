759142bab3342561d30c88dfadb570298e5093a4
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2022-05-31 01:04:33 +0200

    🎉 Add : web utils file

---
+++ OnlineDrinkShop/WebUtils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineDrinkShop
+{
+    public class WebUtils
+    {
+        public const string CurrentUser = "Current User Name";
+        public const int Admin = 1;
+        public const string Cart = "MyCart";
+    }
+}
